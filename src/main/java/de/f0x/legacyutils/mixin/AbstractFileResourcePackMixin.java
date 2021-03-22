04cf86bd0b09b5d231173a24193da2e103591af2
F0X <f0xde@protonmail.com>
2021-03-22 22:40:32 +0100

    Draw to correct image

--- src/main/java/de/f0x/legacyutils/mixin/AbstractFileResourcePackMixin.java
+++ src/main/java/de/f0x/legacyutils/mixin/AbstractFileResourcePackMixin.java
@@ -10,16 +10,16 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 @Mixin(AbstractFileResourcePack.class)
-public class AbstractFileResourcePackMixin {
+public abstract class AbstractFileResourcePackMixin {
     private static final int MAX_SIZE = 64;
 
     @Inject(method = "method_4367", at = @At("RETURN"), cancellable = true)
     void scaleIcon(CallbackInfoReturnable<BufferedImage> cir) {
         BufferedImage original = cir.getReturnValue();
-        if (original.getWidth() > MAX_SIZE) {
-            BufferedImage scaled = new BufferedImage(MAX_SIZE, MAX_SIZE, BufferedImage.TYPE_INT_ARGB);
-            Graphics graphics = original.getGraphics();
-            graphics.drawImage(scaled, 0, 0, MAX_SIZE, MAX_SIZE, null);
+        if (original != null && original.getWidth() > MAX_SIZE) {
+            BufferedImage scaled = new BufferedImage(MAX_SIZE, MAX_SIZE, original.getType());
+            Graphics graphics = scaled.getGraphics();
+            graphics.drawImage(original, 0, 0, MAX_SIZE, MAX_SIZE, null);
             graphics.dispose();
             cir.setReturnValue(scaled);
         }

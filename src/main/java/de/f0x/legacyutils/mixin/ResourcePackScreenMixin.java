ffac09635a79143001b62a050285d6578dea0f12
F0X <f0xde@protonmail.com>
2021-03-23 16:51:38 +0100

    Sort resource packs by name

---
+++ src/main/java/de/f0x/legacyutils/mixin/ResourcePackScreenMixin.java
@@ -0,0 +1,26 @@
+package de.f0x.legacyutils.mixin;
+
+import com.mojang.realmsclient.gui.ChatFormatting;
+import net.minecraft.client.gui.screen.ResourcePackScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.resource.ResourcePackLoader;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+
+@Mixin(ResourcePackScreen.class)
+public class ResourcePackScreenMixin extends Screen {
+    @Redirect(
+        method = "init",
+        at = @At(value = "INVOKE", target = "Ljava/util/List;removeAll(Ljava/util/Collection;)Z")
+    )
+    boolean sortResourcePacks(List<ResourcePackLoader.Entry> packs, Collection<ResourcePackLoader.Entry> selectedPacks) {
+        boolean changed = packs.removeAll(selectedPacks);
+        packs.sort(Comparator.comparing(entry -> ChatFormatting.stripFormatting(entry.getName())));
+        return changed;
+    }
+}
